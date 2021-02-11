2f1c3684d962276784b5038cdfd6586e4336c89b
Mohammad Kamrul Hasan <kamrulhasan59246@gmail.com>
2021-02-11 14:23:33 +0000

    Added Theme changing system

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".mode__switch").onclick = function () {
+  document.body.classList.contains("dark") ? (document.body.classList.remove("dark"), document.body.classList.add("light")) : (document.body.classList.remove("light"), document.body.classList.add("dark"));
+}
+
