ae758dcd3ef46c1adaa959bbe590720757362f4f
Mohammad Kamrul Hasan <kamrulhasan59246@gmail.com>
2021-03-14 01:48:47 +0000

    Pwalized

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,7 @@ document.querySelector(".mode__switch").onclick = function () {
   document.body.classList.contains("dark") ? (document.body.classList.remove("dark"), document.body.classList.add("light")) : (document.body.classList.remove("light"), document.body.classList.add("dark"));
 }
 
+if ("serviceWorker" in navigator) {
+  // register service worker
+  navigator.serviceWorker.register("../service-worker.js");
+}
