fe45afb94247827b269778bc7005cac1accd2da8
Mohammad Kamrul Hasan <kamrulhasan59246@gmail.com>
2021-04-06 06:49:38 +0000

    Did

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,3 @@
 document.querySelector(".mode__switch").onclick = function () {
   document.body.classList.contains("dark") ? (document.body.classList.remove("dark"), document.body.classList.add("light")) : (document.body.classList.remove("light"), document.body.classList.add("dark"));
 }
-
-if ("serviceWorker" in navigator) {
-  // register service worker
-  navigator.serviceWorker.register("../service-worker.js");
-}
